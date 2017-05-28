d4ce5d3154fcd16f376ded15ba7d8a81951603f3
gitnicolas <alizbaba@yahoo.com>
2017-05-28 15:16:55 +0200

    Update ScheduledIlluminanceTimer.lua

--- ScheduledIlluminanceTimer.lua
+++ ScheduledIlluminanceTimer.lua
@@ -1,12 +1,14 @@
 --[[
 %% properties
 5 value
-7 value
 %% weather
 %% events
 %% globals
 --]]
 
+local motionSensorId = 5
+local lightSensorId = 7
+local dimmerId = 23
 local counterName = "UpperStairsTimer"
 local dayBrightness = "49"
 local nightBrightness = "9"
@@ -16,9 +18,9 @@ local maxBrightness = "99"
 local startSource = fibaro:getSourceTrigger()
 if
 	(
-		tonumber(fibaro:getValue(5, "value")) > 0
-		and tonumber(fibaro:getValue(7, "value")) < illuminanceThreshold
-		and fibaro:getValue(23, "value") ~= maxBrightness
+		tonumber(fibaro:getValue(motionSensorId, "value")) > 0
+		and tonumber(fibaro:getValue(lightSensorId, "value")) < illuminanceThreshold
+		and fibaro:getValue(dimmerId, "value") ~= maxBrightness
 	)
 	or startSource["type"] == "other"
 then
@@ -26,11 +28,11 @@ then
 	fibaro:setGlobal(counterName, counter)
 	if counter == 1 then
 		local brightness = nightBrightness
-		fibaro:call(23, "setValue", brightness)
+		fibaro:call(dimmerId, "setValue", brightness)
 	end
 	setTimeout(function()
 		local counter = tonumber(fibaro:getGlobalValue(counterName)) - 1
 		fibaro:setGlobal(counterName, counter)
-		if counter == 0 and fibaro:getValue(23, "value") ~= maxBrightness then fibaro:call(23, "turnOff") end
+		if counter == 0 and fibaro:getValue(dimmerId, "value") ~= maxBrightness then fibaro:call(dimmerId, "turnOff") end
 	end, 80000)
 end
