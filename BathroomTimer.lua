2961190aee3de09ccb0b15336f564983484c4cfb
gitnicolas <alizbaba@yahoo.com>
2017-06-03 08:00:14 +0200

    Update BathroomTimer.lua

--- BathroomTimer.lua
+++ BathroomTimer.lua
@@ -1,1 +1,25 @@
+--[[
+%% properties
+??? value
+%% weather
+%% events
+%% globals
+--]]
 
+local presenceSensorId = ???
+local dimmerId = 237
+local dayStart = 6
+local dayEnd = 21
+local dayBrightness = "99"
+local nightBrightness = "5"
+
+local startSource = fibaro:getSourceTrigger()
+if
+	tonumber(fibaro:getValue(presenceSensorId, "value")) > 0
+	or startSource["type"] == "other"
+then
+	local hour = os.date("*t")["hour"]
+	local brightness
+	if hour > dayStart and hour < dayEnd then brightness = dayBrightness else brightness = nightBrightness end
+	fibaro:call(dimmerId, "setValue", brightness)
+end
